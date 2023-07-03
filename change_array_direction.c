fb89c4f7982e7cd2eb853175fb1161261b6db6e3
TomasNagy <tomi.nagy@outlook.com>
2023-07-03 12:27:59 +0200

    feat: implement change_direction functions

--- change_array_direction.c
+++ change_array_direction.c
@@ -5,16 +5,13 @@
 #define MAX_NUMBERS 5
 #define MAX_STRING 20
 
+void change_int_direction(int *num, int length);
+void change_char_direction(char *string, int length);
+
 int main(void)
 {
     // Initialize arrays to work with
     int numbers[MAX_NUMBERS] = {12,5,9,0,30};
-
-    // Create a string character array; allocate enough memory for the string
-    char *string = malloc(MAX_STRING * sizeof(char));
-
-    // Copy the string into the allocated memory
-    strncpy(string, "Hello world!", MAX_STRING);
 
     // Print numbers array original values
     printf ("Array: [");
@@ -30,6 +27,37 @@ int main(void)
     }
     printf ("]\n");
 
+    // Change direction of the original array
+    change_int_direction(numbers, MAX_NUMBERS);
+
+    // Print numbers array original values
+    printf ("Array: [");
+    for (int i = 0; i < MAX_NUMBERS; i++)
+    {
+        if (i == 0)
+        {
+            printf ("%i", numbers[i]);
+            continue;
+        }
+        
+        printf (", %i", numbers[i]);
+    }
+    printf ("]\n");
+
+
+
+    // Create a string character array; allocate enough memory for the string
+    char *string = malloc(MAX_STRING * sizeof(char));
+
+    // Copy the string into the allocated memory
+    strncpy(string, "Hello world!", MAX_STRING);
+
+    // Print character array original values
+    printf ("Array: %s\n", string);
+
+    // Change direction of the original array
+    change_char_direction(string, strlen(string));
+
     // Print character array original values
     printf ("Array: %s\n", string);
 
@@ -37,4 +65,50 @@ int main(void)
     free(string);
 
     return EXIT_SUCCESS;
+}
+
+void change_int_direction(int *num, int length)
+{
+    // Set pointer to the arrays' last elements' address
+    int *end_of_array = num + (length - 1);
+
+    // Swap values until num and end_of_array address is the same or
+    // num's address is higher then end_of_array's address
+    while (num < end_of_array)
+    {
+        // Swap the values, don't forget to dereference the pointer to get the
+        // values and not the addresses
+     
+        int temp = *num;
+        *num = *end_of_array;
+        *end_of_array = temp;
+
+        // Increment num address and decrement end_of_array address
+        // Step one by one to the middle of the array
+        num++;
+        end_of_array--;
+    }
+}
+
+void change_char_direction(char *string, int length)
+{
+    // Set pointer to the arrays' last elements' address
+    char *end_of_array = string + (length - 1);
+
+    // Swap values until string and end_of_array address is the same or
+    // string's address is higher then end_of_array's address
+    while (string < end_of_array)
+    {
+        // Swap the values, don't forget to dereference the pointer to get the
+        // values and not the addresses
+     
+        char temp = *string;
+        *string = *end_of_array;
+        *end_of_array = temp;
+
+        // Increment string address and decrement end_of_array address
+        // Step one by one to the middle of the array
+        string++;
+        end_of_array--;
+    }
 }
